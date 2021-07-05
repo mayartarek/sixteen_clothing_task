5f64d9fe9cc5617654a226da5bef29f90c64d812
Mayar Tarek <mayar_1-1@windowslive.com>
2021-07-05 02:51:49 +0200

    commit 2

---
+++ contactus.js
@@ -0,0 +1,32 @@
+$(document).ready(
+
+function(
+
+){
+    $("#slidding1").click(function (){
+
+       $("#p-slide1").slideToggle();
+
+    });
+    $("#slidding2").click(function (){
+
+        $("#p-slide2").slideToggle();
+ 
+     });
+ 
+     $("#slidding3").click(function (){
+
+        $("#p-slide3").slideToggle();
+ 
+     });
+     $("#slidding4").click(function (){
+
+        $("#p-slide4").slideToggle();
+ 
+     });
+ 
+ 
+   
+}
+
+);
